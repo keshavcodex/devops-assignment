d4f068eb4f3375170ea65c36773d504eb8eb03e7
keshavcodex <keshavcodex@gmail.com>
2023-10-08 14:11:29 +0530

    updated terraform

--- instance.tf
+++ instance.tf
@@ -1,12 +1,22 @@
-variable "AMI_ID" {
-  description = "this is ami id of aws"
-}
-
+# Configure the AWS provider with your desired region
 provider "aws" {
-  region = "us-east-1" # Set your desired AWS region
+  region = "ap-south-1"  # Replace with your desired AWS region
 }
 
+# Define the EC2 instance resource
 resource "aws_instance" "example" {
-  ami           = var.AMI_ID Replace with your desired AMI ID
-  instance_type = "t2.micro"             # Replace with your desired instance 
+  ami           = "ami-0ad704c126371a549"  
+  instance_type = "t2.micro"     
+
+  # Optional: You can add more configuration options here, such as:
+  # subnet_id      = "subnet-xxxxxxxxxx"
+  # security_groups = ["security-group-name"]
+
+  # Optional: Define key_name if you want to use an existing SSH key pair
+  # key_name       = "your-key-pair-name"
+}
+
+# Output the public IP address of the created instance
+output "instance_public_ip" {
+  value = aws_instance.example.public_ip
 }
